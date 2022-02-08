919761b1915919a919e9215f55700faa68ce4317
Bloop_2007 <deva12jith@gmail.com>
2022-02-08 16:32:37 +0400

    Changed some stuff

--- bot.py
+++ bot.py
@@ -6,8 +6,6 @@ import discord
 import random
 from dotenv import load_dotenv
 
-
-# get the bot token from the .env file
 load_dotenv(dotenv_path= "D:\Coding\DiscordBot/Token.env")
 os.path.abspath(os.path.dirname(__file__))
 
@@ -24,13 +22,11 @@ class MyBot(commands.Bot):
 async def on_ready():
     print('Bot is ready.')
 
-# create a msg when a player  has joined the server and send it to the channel
 @client.event
 async def on_member_join(member):
         channel = client.get_channel(66445656564456565)
         await channel.send(f'{member} has joined the server.')
 
-# create a msg when a player has left the server and send it to the channel
 @client.event
 async def on_member_remove(member):
         channel = client.get_channel(66445656564456565)
@@ -40,7 +36,6 @@ async def on_member_remove(member):
 async def ping(ctx):
     await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
 
-# create a 8ball command
 @client.command(aliases=['8ball', 'test'])
 async def _8ball(ctx, *, question):
     responses = ['It is certain.',
